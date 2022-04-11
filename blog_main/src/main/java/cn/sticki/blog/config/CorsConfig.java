4960309e1dc86bb7c74b44947f898c42c768c612
天文 黄 <1002744113@qq.com>
2022-04-11 16:48:43 +0800

    添加跨域配置文件

---
+++ blog_main/src/main/java/cn/sticki/blog/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package cn.sticki.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOriginPatterns("*").allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS").allowCredentials(true).maxAge(3600).allowedHeaders("*");
+		System.out.println("CorsConfig");
+	}
+
+}
