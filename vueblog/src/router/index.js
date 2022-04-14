5ef8b9b856a1773be9310dbd69a307fe4086ab60
Maverick-72 <2983885355@qq.com>
2022-04-14 14:15:48 +0800

    页面修改

--- vueblog/src/router/index.js
+++ vueblog/src/router/index.js
@@ -6,20 +6,20 @@ import Login from "@/views/Login";
 import Blogs from "@/views/Blogs";
 import BlogEdit from "@/views/BlogEdit";
 import BlogDetail from "@/views/BlogDetail";
-
-
+import P_Register from "@/views/P_Register";
+import P_Register2 from "@/views/P_Register2";
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     name: 'Index',
-    redirect:{name:"Blogs"}
+    redirect:{name:"P_Register2"}
   },
   {
-    path: '/blogs',
-    name: 'Blogs',
-    component: Blogs
+    path: '/P_Register2',
+    name: 'P_Register2',
+    component: P_Register2
   },
   {
     path: '/login',
@@ -27,9 +27,23 @@ const routes = [
     component: Login
   },
   {
+    path: '/P_Register',
+    name: 'P_Register',
+    component: P_Register
+  },
+  {
     path: '/register',
     name: 'register',
     component: Register
+  },
+  {
+    path: '/blogs',
+    name: 'Blogs',
+    component: Blogs
+  },{
+    path: '/blog/add',
+    name: 'BlogEdit',
+    component: BlogEdit
   },
   {
     path: '/blog/add',
