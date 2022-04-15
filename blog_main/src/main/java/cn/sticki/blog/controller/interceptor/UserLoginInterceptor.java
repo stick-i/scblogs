ef71aa9d95ece5df86c8d0027c02b27eb059fa12
天文 黄 <1002744113@qq.com>
2022-04-15 11:23:55 +0800

    添加了往Response中Object转json的方法

--- blog_main/src/main/java/cn/sticki/blog/controller/interceptor/UserLoginInterceptor.java
+++ blog_main/src/main/java/cn/sticki/blog/controller/interceptor/UserLoginInterceptor.java
@@ -1,6 +1,9 @@
 package cn.sticki.blog.controller.interceptor;
 
+import cn.sticki.blog.pojo.vo.RestTemplate;
+import cn.sticki.blog.util.ResponseUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +13,9 @@ import javax.servlet.http.HttpSession;
 @Slf4j
 public class UserLoginInterceptor implements HandlerInterceptor {
 
+    @Autowired
+    private ResponseUtils responseUtils;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         log.debug("UserLoginInterceptor,url->{}", request.getRequestURL());
@@ -17,7 +23,7 @@ public class UserLoginInterceptor implements HandlerInterceptor {
         if (session.getAttribute("user") != null) {
             return true;
         }
-        response.getWriter().println("未登录");
+        responseUtils.objectToJson(response, new RestTemplate(400, "未登录"));
         return false;
     }
 
