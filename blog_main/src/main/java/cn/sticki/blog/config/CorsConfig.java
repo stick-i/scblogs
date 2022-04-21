df1bcbd026cd026ce2b9e6f1572c012feda03b7b
stick-i <1002744113@qq.com>
2022-04-21 09:51:37 +0800

    请求方式更改为ssl，解决cookie被浏览器拦截问题；更新接口文档；

--- blog_main/src/main/java/cn/sticki/blog/config/CorsConfig.java
+++ blog_main/src/main/java/cn/sticki/blog/config/CorsConfig.java
@@ -1,18 +1,32 @@
 package cn.sticki.blog.config;
 
+import org.springframework.boot.autoconfigure.session.DefaultCookieSerializerCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.session.MapSessionRepository;
+import org.springframework.session.SessionRepository;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * 解决跨域问题
+ * 解决session被浏览器拦截的问题
  */
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+@EnableSpringHttpSession
+public class CorsConfig {
 
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOriginPatterns("*").allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS").allowCredentials(true).maxAge(3600).allowedHeaders("*");
+	@Bean
+	public SessionRepository sessionRepository() {
+		return new MapSessionRepository(new ConcurrentHashMap<>());
+	}
+
+	@Bean
+	DefaultCookieSerializerCustomizer cookieSerializerCustomizer() {
+		return cookieSerializer -> {
+			cookieSerializer.setSameSite("None");
+			cookieSerializer.setUseSecureCookie(true); // 此项必须，否则set-cookie会被chrome浏览器阻拦
+		};
 	}
 
 }
