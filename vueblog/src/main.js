68d569df29689f2e4f4d8fb99b04bf44ac3a0745
Maverick-72 <2983885355@qq.com>
2022-04-23 22:46:34 +0800

    个人博客信息页面创建

--- vueblog/src/main.js
+++ vueblog/src/main.js
@@ -9,10 +9,14 @@ import mavonEditor from 'mavon-editor'
 
 import ElementUI from 'element-ui';
 import jquery from 'jquery';
+// import $ from 'jquery'
+
+// import 'bootstrap'
 // import jquery from 'jquery-3.6.0.js';
 import 'element-ui/lib/theme-chalk/index.css';
 import 'mavon-editor/dist/css/index.css'
-
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/js/bootstrap.min.js'
 import "./axios"
 
 import "./assets/css/iconfont/iconfont.css"
@@ -26,7 +30,7 @@ Vue.use(mavonEditor);
 axios.defaults.withCredentials = true;
 Vue.prototype.$axios = axios;
 Vue.prototype.$jquery = jquery;
-
+// Vue.prototype.$ = jquery;
 // 兄弟组件通信
 Vue.prototype.bus = new Vue()
 
--- vueblog/vue.config.js
+++ vueblog/vue.config.js
@@ -2,3 +2,4 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+const webpack = require("webpack")
