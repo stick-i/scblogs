765c1040d05556d4cb8eff03986a460e4ff0a7f9
Maverick-72 <2983885355@qq.com>
2022-04-28 22:33:50 +0800

    修改接口的各种问题

--- vueblog/src/axios.js
+++ vueblog/src/axios.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-axios.defaults.baseURL="https://local.sticki.cn/api/v1"
-
+// axios.defaults.baseURL="https://local.sticki.cn/api/v1"
+axios.defaults.baseURL="/api"
--- vueblog/vue.config.js
+++ vueblog/vue.config.js
@@ -3,3 +3,17 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 const webpack = require("webpack")
+module.exports = {
+
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://local.sticki.cn/api/v1',// 后端接口
+        changeOrigin: true, // 是否跨域
+        pathRewrite: {
+          '/api': ''
+        }
+      }
+    }
+  }
+}
