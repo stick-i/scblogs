7ad1514f5921f85f13de8ed75204d0345910dc00
黄天文 <1002744113@qq.com>
2022-05-22 19:26:19 +0800

    添加消息服务模块 message-service。包含发送信息的功能（目前只支持邮件）

---
+++ message-server/src/main/java/cn/sticki/message/MessageServerApplication.java
@@ -0,0 +1,13 @@
+package cn.sticki.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageServerApplication.class, args);
+	}
+
+}
