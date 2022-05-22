71928bf616c3bff42e76c99bad40c7d547c7d875
黄天文 <1002744113@qq.com>
2022-05-22 19:36:40 +0800

    添加用户服务模块 user-server。包含用户相关的一些接口，都是原来有的接口功能。

---
+++ user-server/src/main/java/cn/sticki/user/controller/UserController.java
@@ -0,0 +1,135 @@
+package cn.sticki.user.controller;
+
+import cn.sticki.common.enumeration.CacheSpace;
+import cn.sticki.common.enumeration.type.FileType;
+import cn.sticki.common.pojo.RestResult;
+import cn.sticki.common.utils.FileUtils;
+import cn.sticki.user.pojo.User;
+import cn.sticki.user.service.UserService;
+import com.alicp.jetcache.Cache;
+import com.alicp.jetcache.anno.CreateCache;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	@Resource
+	private UserService userService;
+
+	// @CreateCache(name = CacheSpace.Login_UserID)
+	// Cache<Integer, User> cache;
+
+	@CreateCache(name = CacheSpace.UserService_SendMailTime, expire = 300)
+	private Cache<Integer, Long> sendMailTimeCache;
+
+	/**
+	 * 获取公开信息
+	 *
+	 * @param username 用户名
+	 */
+	@GetMapping
+	public RestResult<Object> getByUsername(String username, @RequestHeader(required = false) Integer id) {
+		User getUser = null;
+		if ((username == null || username.length() == 0) && id != null) {
+			getUser = userService.getById(id);
+			log.debug("getByUsername, sessionUser ,user->{}", getUser);
+		} else if (username != null && username.length() != 0) {
+			getUser = userService.getByUsername(username);
+			log.debug("getByUsername, userService.getByUsername ,user->{}", getUser);
+		}
+		return new RestResult<>(getUser);
+	}
+
+	/**
+	 * 修改昵称
+	 *
+	 * @param nickname 昵称
+	 */
+	@PutMapping("/nickname")
+	public RestResult<Object> updateNickname(@NotNull String nickname, @RequestHeader Integer id) {
+		if (userService.updateNickname(id, nickname)) {
+			// cache.put(id, user);
+			return new RestResult<>(true);
+		}
+		return new RestResult<>(false);
+	}
+
+	/**
+	 * 修改头像
+	 *
+	 * @param avatarFile 文件流
+	 */
+	@PutMapping("/avatar")
+	public RestResult<Object> updateAvatar(@NotNull MultipartFile avatarFile, @RequestHeader Integer id) {
+		log.debug("updateAvatar,fileSize->{}", avatarFile.getSize());
+		// 检查文件，小于1Mib ,仅支持JPEG和PNG
+		FileUtils.checkFile(avatarFile, 1024 * 1024L, FileType.JPEG, FileType.PNG);
+		// cache.put(id, user);
+		return new RestResult<>(userService.updateAvatar(id, avatarFile));
+	}
+
+	/**
+	 * 修改密码
+	 *
+	 * @param oldPassword 旧密码
+	 * @param newPassword 新密码
+	 */
+	@PutMapping("/password")
+	public RestResult<Object> updatePassword(@NotNull String oldPassword, @NotNull String newPassword, @RequestHeader Integer id) {
+		if (userService.checkPassword(id, oldPassword)) {
+			boolean result = userService.updatePasswordById(id, newPassword);
+			return new RestResult<>(result);
+		}
+		return new RestResult<>(false, "修改失败");
+	}
+
+	/**
+	 * 修改邮箱
+	 *
+	 * @param mail       新邮箱
+	 * @param mailVerify 邮箱验证码
+	 */
+	@PutMapping("/mail")
+	public RestResult<Object> updateMail(@NotNull String mail, @NotNull String mailVerify, @RequestHeader Integer id) {
+		if (userService.checkMailVerify(id, mailVerify)) {
+			boolean result = userService.updateMail(id, mail);
+			return new RestResult<>(result);
+		}
+		return new RestResult<>(false, "修改失败");
+	}
+
+	/**
+	 * 发送邮箱验证码
+	 */
+	@PostMapping("/mail/send-mail-verify")
+	public RestResult<Object> sendMailVerifyForUpdateMail(@RequestHeader Integer id) {
+		Long sendTime = sendMailTimeCache.get(id);
+		Long nowTime = System.currentTimeMillis() / 1000;
+		// 判断是否发送过邮件，若上一次发送邮件的时间超过60s则允许发送
+		if (sendTime == null || nowTime - sendTime > 60) {
+			boolean result = userService.sendMailVerify(id);
+			if (result) {
+				sendMailTimeCache.put(id, nowTime);// 将发送邮件的时间存到Cache(Redis)
+				return new RestResult<>(true, "发送成功");
+			}
+		}
+		return new RestResult<>(false, "发送失败");
+	}
+
+	/**
+	 * 删除用户
+	 */
+	@DeleteMapping("/user")
+	public RestResult<Object> delete(@NotNull String password, @RequestHeader Integer id) {
+		if (!userService.checkPassword(id, password)) return new RestResult<>(false, "密码错误");
+		else return new RestResult<>(userService.removeById(id));
+	}
+
+}
