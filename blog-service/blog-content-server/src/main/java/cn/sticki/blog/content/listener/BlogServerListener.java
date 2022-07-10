97570c2a7d11faa0a7d933f28d9952c2dc46720e
黄天文 <1002744113@qq.com>
2022-07-10 19:41:10 +0800

    feat(blog-content-server): 添加了基于MQ来同步博客数据的功能
    添加了保存博客和删除博客的MQ监听器，用于实现博客数据的同步。

---
+++ blog-service/blog-content-server/src/main/java/cn/sticki/blog/content/listener/BlogServerListener.java
@@ -0,0 +1,60 @@
+package cn.sticki.blog.content.listener;
+
+import cn.sticki.blog.content.mapper.BlogRepository;
+import cn.sticki.blog.content.pojo.BlogDoc;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+import static cn.sticki.blog.sdk.BlogMqConstants.*;
+
+/**
+ * 博客服务消息队列监听器
+ *
+ * @author 阿杆
+ * @version 1.0
+ * @date 2022/7/10 9:32
+ */
+@Slf4j
+@Component
+public class BlogServerListener {
+
+	@Resource
+	private BlogRepository blogRepository;
+
+	/**
+	 * 保存博客队列
+	 */
+	private static final String BLOG_SAVE_QUEUE = "blog.save.es.queue";
+
+	/**
+	 * 删除博客队列
+	 */
+	private static final String BLOG_DELETE_QUEUE = BLOG_DELETE_KEY + ".es.queue";
+
+	@RabbitListener(bindings = @QueueBinding(
+			exchange = @Exchange(name = BLOG_EXCHANGE),
+			value = @Queue(name = BLOG_SAVE_QUEUE),
+			key = {BLOG_INSERT_KEY, BLOG_UPDATE_KEY}
+	))
+	public void saveListener(BlogDoc blogDoc) {
+		log.debug("save blogDoc，{}", blogDoc);
+		blogRepository.save(blogDoc);
+	}
+
+	@RabbitListener(bindings = @QueueBinding(
+			exchange = @Exchange(name = BLOG_EXCHANGE),
+			value = @Queue(name = BLOG_DELETE_QUEUE),
+			key = BLOG_DELETE_KEY
+	))
+	public void deleteListener(Long blogId) {
+		log.debug("delete blog ,id->{}", blogId);
+		blogRepository.deleteById(blogId);
+	}
+
+}
