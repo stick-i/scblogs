4b9a2a3a6492ca09ece656306bb501962fab1949
黄天文 <1002744113@qq.com>
2022-08-16 09:09:51 +0800

    fix(vueblog): 登录密码长度限制解除

--- vueblog/src/views/login/childComps/LoginItem.vue
+++ vueblog/src/views/login/childComps/LoginItem.vue
@@ -59,13 +59,13 @@ export default {
       },
       rules: {
         username: [
-          { required: true, message: "请输入用户名", trigger: "blur" },
-          { min: 1, max: 16, message: "长度在 1 到 16 个字符", trigger: "blur" },
-        ],
+					{required: true, message: "请输入用户名", trigger: "blur"},
+					{min: 1, max: 20, message: "长度在 1 到 20 个字符", trigger: "blur"},
+				],
         password: [
-          { required: true, message: "请输入密码", trigger: "blur" },
-          { min: 6, max: 20, message: "长度在 6 到 20 个字符", trigger: "blur" },
-        ],
+					{required: true, message: "请输入密码", trigger: "blur"},
+					{min: 1, max: 20, message: "长度在 1 到 20 个字符", trigger: "blur"},
+				],
       },
     };
   },
