3246100ca0ae962289cef45e8791152b0a234a7c
Sticki <sticki@qq.com>
2022-09-24 21:00:18 +0800

    perf(user-server): 引入EnableDefaultResponseAdvice，并修改了UserController的返回值代码，更加贴近RestResult的使用方法

--- user-service/user-server/src/main/java/cn/sticki/user/UserServerApplication.java
+++ user-service/user-server/src/main/java/cn/sticki/user/UserServerApplication.java
@@ -3,6 +3,7 @@ package cn.sticki.user;
 import cn.sticki.common.redis.autoconfig.EnableRedisSerialize;
 import cn.sticki.common.tool.mybatisconfig.EnableMybatisPlusIPage;
 import cn.sticki.common.web.advice.EnableDefaultExceptionAdvice;
+import cn.sticki.common.web.advice.EnableDefaultResponseAdvice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -17,6 +18,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableFeignClients(basePackages = {"cn.sticki.resource.client", "cn.sticki.message.client"}) // 开启feign
 @EnableMybatisPlusIPage // 开启mybatis分页助手
 @EnableDefaultExceptionAdvice // 默认异常处理
+@EnableDefaultResponseAdvice // 开启默认的响应体包装处理器
 @EnableRedisSerialize // 开启RedisTemplate序列化配置
 public class UserServerApplication {
 
