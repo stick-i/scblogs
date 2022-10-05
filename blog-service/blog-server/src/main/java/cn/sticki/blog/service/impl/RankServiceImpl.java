3dbcaf4c70be57739a729058aa2b1c9b7cf878cf
Sticki <sticki@qq.com>
2022-10-05 19:28:50 +0800

    style: 抽离 getDatKey和gatWeekKey 方法

--- blog-service/blog-server/src/main/java/cn/sticki/blog/service/impl/RankServiceImpl.java
+++ blog-service/blog-server/src/main/java/cn/sticki/blog/service/impl/RankServiceImpl.java
@@ -116,29 +116,4 @@ public class RankServiceImpl implements RankService {
 		return result;
 	}
 
-    /**
-     * 获取 day key
-     */
-    private long getDayKey() {
-        return System.currentTimeMillis() / (1000 * 60 * 60 * 24);
-    }
-
-    /**
-     * 获取上一周的 week key
-     */
-    private long getWeekKey() {
-        // 拿到 week key,和day key
-        long week = System.currentTimeMillis() / (1000 * 60 * 60 * 24 * 7);
-        long day = getDayKey();
-        // 由于原来的week key并不是从周一开始算起，所以进行移位计算，拿到上周的week key
-        long key = day % 7;
-        if (key >= 4) {
-            // 经计算，取模值大于等于 4 时 进入新的一周，将week key-1
-            return week - 1;
-        }
-        //否则，在新的一周的周四时，week值将再加1，所以要拿到上一周的week key需要-2
-        return week - 2;
-    }
-
-
 }
