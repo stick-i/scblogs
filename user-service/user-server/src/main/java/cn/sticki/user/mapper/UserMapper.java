60a5cf90175844fc6e88ca2711e5ba7c29e0a5b2
阿杆 <sticki@qq.com>
2023-01-14 22:11:53 +0800

    fix: 处理user表和user_safety表的唯一索引和逻辑删除造成的冲突。

--- user-service/user-server/src/main/java/cn/sticki/user/mapper/UserMapper.java
+++ user-service/user-server/src/main/java/cn/sticki/user/mapper/UserMapper.java
@@ -4,10 +4,21 @@ import cn.sticki.user.pojo.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.io.Serializable;
+
 /**
  * @author 阿杆
  */
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
+
+	/**
+	 * 根据 ID 删除
+	 *
+	 * @param id 主键ID
+	 * @return 影响数据行
+	 */
+	@Override
+	int deleteById(Serializable id);
 
 }
