95c2ee326ccc5b993a7834b597807b18409de520
阿杆 <sticki@qq.com>
2023-01-18 22:25:29 +0800

    perf: 优化全局异常处理器，并在抛出异常时手动保存访问记录

--- gateway-service/src/main/java/cn/sticki/gateway/handler/GlobalExceptionHandler.java
+++ gateway-service/src/main/java/cn/sticki/gateway/handler/GlobalExceptionHandler.java
@@ -1,12 +1,13 @@
 package cn.sticki.gateway.handler;
 
 import cn.sticki.common.result.RestResult;
-import cn.sticki.gateway.service.VisitService;
+import cn.sticki.gateway.service.VisitRecordService;
+import cn.sticki.gateway.utils.MonoUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebExchange;
@@ -25,32 +26,36 @@ import javax.annotation.Resource;
 public class GlobalExceptionHandler implements ErrorWebExceptionHandler {
 
 	@Resource
-	private VisitService visitService;
+	private VisitRecordService visitRecordService;
 
 	@Override
 	public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
 		ServerHttpResponse response = exchange.getResponse();
-
-		// 由于抛出异常后不会再经过 IpLimitFilter，也不会保存访问记录，故手动在此进行调用
-		boolean isLimit = visitService.checkLimit(exchange);
-		if (isLimit) {
-			return visitService.buildMonoWrap(response, RestResult.limit());
-		}
+		ServerHttpRequest request = exchange.getRequest();
 
 		if (response.isCommitted()) {
 			return Mono.error(ex);
 		}
 
-		// HTTP状态码异常
-		response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+		// 设置返回信息和HTTP状态码
+		RestResult<Object> result;
+		HttpStatus httpStatus;
 		if (ex instanceof ResponseStatusException) {
-			HttpStatus httpStatus = ((ResponseStatusException) ex).getStatus();
-			response.setStatusCode(httpStatus);
-			return visitService.buildMonoWrap(response, RestResult.fail(httpStatus.getReasonPhrase()));
+			// HTTP状态码异常
+			log.warn("Http Status Warn : {}, url: {}", ex.getMessage(), request.getURI());
+			httpStatus = ((ResponseStatusException) ex).getStatus();
+		} else {
+			// 系统异常
+			log.error("Error Gateway, {}: {}, url: {}", ex.getClass(), ex.getMessage(), request.getURI());
+			httpStatus = HttpStatus.BAD_GATEWAY;
 		}
+		result = RestResult.fail(httpStatus.getReasonPhrase(), httpStatus.value());
+		response.setStatusCode(httpStatus);
 
-		log.warn("Warn Gateway : {}: {}, {}", ex.getClass(), ex.getMessage(), exchange.getRequest().getPath());
-		return visitService.buildMonoWrap(response, RestResult.notFound());
+		// 由于系统异常，不会正常走过滤器，故手动保存访问记录
+		visitRecordService.saveRecord(exchange);
+
+		return MonoUtils.buildMonoWrap(response, result);
 	}
 
 }
